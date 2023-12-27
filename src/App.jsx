3a2065c82a694fe8aa47b15215f04b4987171298
kalpizard <kalpizard@fwdcostarica.com>
2023-12-27 14:21:50 -0600

    actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Dashboard from "./components/Dashboard";
+import Home from "./Home";
+import Dashboard from "./Dashboard";
 
 export default class App extends Component {
   render() {
